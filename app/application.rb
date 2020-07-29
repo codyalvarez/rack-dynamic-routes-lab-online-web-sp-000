3256f77d01328f7104d8018516918367ea5b8435
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-29 23:28:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,27 +2,30 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
-    # returns 404 for a bad route
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item.price}"
-      end
-    elsif req.path.match(/search/)
-
-        search_term = req.params["q"]
-
-      elsif @@items.include?(search_term)
-        resp.write "#{search_term}"
-      elsif @@items.include?!(search_term)
-        resp.write "Route not found"
-        resp.status 404
-      else
-        resp.write "Route not found"
-        resp.status 400
-      end
-
-      resp.finish
-    end
   end
 end
+
+
+
+#   # returns 404 for a bad route
+#   if req.path.match(/items/)
+#     @@items.each do |item|
+#       resp.write "#{item.price}"
+#     end
+#   elsif req.path.match(/search/)
+#
+#       search_term = req.params["q"]
+#
+#     elsif @@items.include?(search_term)
+#       resp.write "#{search_term}"
+#     elsif @@items.include?!(search_term)
+#       resp.write "Route not found"
+#       resp.status 404
+#     else
+#       resp.write "Route not found"
+#       resp.status 400
+#     end
+#
+#     resp.finish
+#   end
+# end
