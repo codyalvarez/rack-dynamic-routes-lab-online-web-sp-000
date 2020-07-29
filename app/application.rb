3edee511aaf08b95027ff930528c067e32ece900
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-29 23:48:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -19,24 +19,3 @@ class Application
   end
 end
 
-#   if req.path.match(/items/)
-#     @@items.each do |item|
-#       resp.write "#{item.price}"
-#     end
-#   elsif req.path.match(/search/)
-#
-#       search_term = req.params["q"]
-#
-#     elsif @@items.include?(search_term)
-#       resp.write "#{search_term}"
-#     elsif @@items.include?!(search_term)
-#       resp.write "Route not found"
-#       resp.status 404
-#     else
-#       resp.write "Route not found"
-#       resp.status 400
-#     end
-#
-#     resp.finish
-#   end
-# end
