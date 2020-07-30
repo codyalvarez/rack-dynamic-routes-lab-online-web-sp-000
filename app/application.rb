7f970f03ceb68e32da68a739b53a466617c62549
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-30 00:30:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
     if req.path.match(/items/) && @@items.include?(@@item)
         resp.write "#{@@item.price}"\
-      elsif @@items.include?!(@@item)
+      elsif !@@items.include?(@@item)
         resp.write "Item not found"
         resp.status = 400
       else
