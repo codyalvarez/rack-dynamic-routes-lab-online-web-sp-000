0276348a3b2a792c9d03578051f58053931c9afe
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-30 00:35:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,11 +6,13 @@ class Application
     @@items = []
     @@item = Item.new(:name, :price)
 
-    if req.path.match(/items/) && @@items.include?(@@item)
-        resp.write "#{@@item.price}"\
+    if req.path.match(/items/)
+      if @@items.include?(@@item)
+        resp.write "#{@@item.price}"
       elsif !@@items.include?(@@item)
         resp.write "Item not found"
         resp.status = 400
+      end
       else
         resp.write "Route not found"
         resp.status = 404
